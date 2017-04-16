c320aaf411f785df8e9aa3b4e66253f4ddad1a7d
Mark Greenwood <mrkjgreenwood@gmail.com>
2017-04-15 22:53:10 -0700

    Initial commit

---
+++ src/Component/ColorList.js
@@ -0,0 +1,46 @@
+import React, { PropTypes } from 'react';
+import StarRating from './StarRating';
+
+const ColorList = ({colors=[], onRate=f=>f, onRemove=f=>f}) => {
+  return (
+    <div className="color-list">
+      {(colors.length === 0) ?
+        <p>No Colors Listed (Add a Color)</p> : 
+        colors.map((color) => <Color key={color.id}
+          { ...color }
+          onRate={(rating) => onRate(color.id, rating)}
+          onRemove={() => onRemove(color.id)} />)
+      }
+    </div>
+  );
+};
+
+ColorList.propTypes = {
+  colors: PropTypes.array,
+  onRate: PropTypes.func,
+  onRemove: PropTypes.func,
+};
+
+export default ColorList;
+
+const Color = ({title, color, rating=0, onRemove=f=>f, onRate=f=>f}) => {
+  return (
+    <section className="color">
+      <h1>{title}</h1>
+      <button onClick={onRemove}>X</button>
+      <div className="color" style={{backgroundColor: color}}>
+      </div>
+      <div>
+        <StarRating starsSelected={rating} onRate={onRate}/>
+      </div>
+    </section>
+  );
+};
+
+Color.propTypes = {
+  title: PropTypes.string,
+  color: PropTypes.string,
+  rating: PropTypes.number,
+  onRemove: PropTypes.func,
+  onRate: PropTypes.func,
+};
