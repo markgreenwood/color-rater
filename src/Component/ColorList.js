13596ffb7e0570077fbec4dfaafa8441f64e0d3c
Mark Greenwood <mark.greenwood@cascadeenergy.com>
2017-04-28 09:08:29 -0700

    Added some of index.js for store; import PropTypes from own module, not React

--- src/Component/ColorList.js
+++ src/Component/ColorList.js
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import StarRating from './StarRating';
 import './ColorList.scss';
 import './Color.scss';
@@ -7,7 +8,7 @@ const ColorList = ({colors=[], onRate=f=>f, onRemove=f=>f}) => {
   return (
     <div className="color-list">
       {(colors.length === 0) ?
-        <p>No Colors Listed (Add a Color)</p> : 
+        <p>No Colors Listed (Add a Color)</p> :
         colors.map((color) => <Color key={color.id}
           { ...color }
           onRate={(rating) => onRate(color.id, rating)}
---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import { createStore, combineReducers } from 'redux';
+import { colors, sort } from './reducers';
+
+const store = createStore(
+  combineReducers({ colors, sort });
+);
+
+console.log(store.getState());
