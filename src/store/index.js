829452e207660e1de907ea7108b968e1c6891335
Mark Greenwood <mark.greenwood@cascadeenergy.com>
2017-05-02 09:12:50 -0700

    Added storeFactory

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,19 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { colors, sort } from './reducers';
+import stateData from '../../data/initialState';
 
-const store = createStore(
-  combineReducers({ colors, sort })
-);
+const storeFactory = (initialState=stateData) => {
+  applyMiddleware(logger, saver)(createStore)(combineReducers({ colors, sort }),
+    (localStorage['redux-store']) ?
+      JSON.parse(localStorage['redux-store']) :
+      initialState
+  );
+};
 
-console.log(store.getState());
+export default storeFactory;
+
+// const store = createStore(
+//   combineReducers({ colors, sort })
+// );
+//
+// console.log(store.getState());
