45b45d62f1196a1b8e3392c91238aee2fe59d1ea
Mark Greenwood <mrkjgreenwood@gmail.com>
2017-05-29 22:14:28 -0700

    Added menu/page template

--- src/components/ui/Page.js
+++ src/components/ui/Page.js
@@ -1,7 +1,39 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
 import HomeIcon from 'react-icons/lib/fa/home';
 import { Link } from 'react-router';
+// import '../../../stylesheets/Page.scss';
 
 const selectedStyle = {
   backgroundColor: 'white',
   color: 'slategray'
 };
+
+const MainMenu = () => (
+  <nav className='main-menu'>
+    <Link to='/'>
+      <HomeIcon/>
+    </Link>
+    <Link to='about' activeStyle={selectedStyle}>
+      [About]
+    </Link>
+    <Link to='events' activeStyle={selectedStyle}>
+      [Events]
+    </Link>
+    <Link to='products' activeStyle={selectedStyle}>
+      [Products]
+    </Link>
+    <Link to='contact' activeStyle={selectedStyle}>
+      [Contact]
+    </Link>
+  </nav>
+);
+
+const Page = ({children}) => (
+  <div className="page">
+    <MainMenu/>
+    {children}
+  </div>
+);
+
+export default Page;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Router, Route, hashHistory } from 'react-router';
 import { Home, About, Events, Products, Contact, Whoops404 } from './components/ui/Pages';
-
+import Page from './components/ui/Page';
 // import { Provider } from 'react-redux';
 // import App from './components/App';
 // import storeFactory from './store';
@@ -14,10 +14,12 @@ window.React = React;
 render(
   <Router history={hashHistory}>
     <Route path="/" component={Home} />
-    <Route path="/about" component={About} />
-    <Route path="/events" component={Events} />
-    <Route path="/products" component={Products} />
-    <Route path="/contact" component={Contact} />
+    <Route path="/" component={Page}>
+      <Route path="/about" component={About} />
+      <Route path="/events" component={Events} />
+      <Route path="/products" component={Products} />
+      <Route path="/contact" component={Contact} />
+    </Route>
     <Route path="*" component={Whoops404} />
   </Router>,
   document.getElementById('react-container')
